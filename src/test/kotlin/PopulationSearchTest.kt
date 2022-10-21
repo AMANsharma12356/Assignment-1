44fdce40b43ca391938789b57b629b587ba54f99
AMANsharma12356 <imamansharma121@gmail.com>
2022-10-21 13:00:22 +0530

    descriptive comment for code changes

---
+++ src/test/kotlin/PopulationSearchTest.kt
@@ -0,0 +1,18 @@
+
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import kotlin.String as String1
+
+class PopulationSearchTest {
+    @Test
+    fun findPopulation() {
+        //Arrange
+        val city: String1 = "Banglore"
+        val expextedPopulationCount = 200000
+        //Act
+        val count = PopulationSearch.getPopulation(city)
+        //Assert
+        Assertions.assertEquals(expextedPopulationCount, count)
+    }
+}
